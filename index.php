5e432aff19dc39228856513b6f2a2eaa05b59115
haycristhine <hayde.cristhine@gmail.com>
2015-06-03 18:32:59 -0300

    Layout Completo da página de login, importação dos frameworks Signed-off-by: haycristhine <hayde.cristhine@gmail.com>

---
+++ index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE HTML>
+
+
+<html>
+    <head>
+        <meta http-equiv = "Content-Type" content = "text/html;charset=utf-8"/>
+        <link type="text/css" rel="stylesheet" href="assets/css/loginpage.css">
+        <title>Login</title>
+        <link rel="icon" type="image/png" href="assets/image/favicon.png">
+    </head>
+
+    <body>
+    <div id="titulo">Talkative</div>
+    <div class="login">
+        <div class="heading">
+            <h2>Sign in</h2>
+            <form action="#">
+
+                <div class="input-group input-group-lg">
+                    <span class="input-group-addon"><i class="fa fa-user"></i></span>
+                    <input type="text" class="form-control" placeholder="Username or email">
+                </div>
+
+                <div class="input-group input-group-lg">
+                    <span class="input-group-addon"><i class="fa fa-lock"></i></span>
+                    <input type="password" class="form-control" placeholder="Password">
+                </div>
+
+                <button type="submit" class="float">Login</button>
+                <button type="submit" class="float">New Account</button>
+            </form>
+        </div>
+    </div>
+    </body>
+</html>
